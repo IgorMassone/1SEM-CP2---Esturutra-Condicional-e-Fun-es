b3aa82d4e92751e384e89359f125e675acff73e3
Carlos Eduardo Affonso <rm569676@fiap.com.br>
2026-04-17 11:06:15 -0300

    Update pcp_1sem_cp2_ex03.py
    Questão 3 - FIAP - Revisada e Corrigida.1

--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -6,22 +6,21 @@ sp2=float(input("Digite a nota da sua Sprint 2: "))
 GS=float(input("Digite a nota da sua GS: "))
 
 if cp1<cp2 and cp1<cp3:
-    media1=(cp2 + cp3 + sp1+ sp2)* 0.4 /4
-    mediapeso=media1 +(GS*0.6)
+    mediasempeso1 = (cp2 + cp3 + sp1 + sp2)/4
+    mediapeso= mediasempeso1*0.4 + GS*0.6
     print()
-    print(f"A sua média sem os pesos é {media1:.2f}")
+    print(f"A sua média sem os pesos é {mediasempeso1:.2f}")
     print(f'A sua média com os pesos é {mediapeso:.2f}')
 if cp2<cp1 and cp2<cp3:
-    media2 = (cp1 + cp3 + sp1 + sp2)* 0.4 /4
-    mediapeso2 = media2 + (GS * 0.6)
+    mediasempeso2 = (cp1 + cp3 + sp1 + sp2)/4
+    mediapeso2 = mediasempeso2 * 0.4 + (GS * 0.6)
     print()
-    print(f"A sua média sem os pesos é {media2:.2f}")
+    print(f"A sua média sem os pesos é {mediasempeso2:.2f}")
     print(f'A sua média com os pesos é {mediapeso2:.2f}')
 if cp3<cp1 and cp3<cp2:
-    media3 = (cp1 + cp2 + sp1 + sp2)* 0.4 /4
-    mediapeso3 = media3 + (GS * 0.6)
+    mediasempeso3 = (cp1 + cp2 + sp1 + sp2)/4
+    mediapeso3 = mediasempeso3 * 0.4 + (GS * 0.6)
     print()
-    print(f"A sua média sem os pesos é {media3:.2f}")
+    print(f"A sua média sem os pesos é {mediasempeso3:.2f}")
     print(f'A sua média com os pesos é {mediapeso3:.2f}')
     #aaaaaaaaaaaaaaa
-
