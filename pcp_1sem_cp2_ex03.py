9602b62c49607640d627b0131a08db02c7e4ae0d
Carlos Eduardo Affonso <rm569676@fiap.com.br>
2026-04-17 11:24:20 -0300

    Update pcp_1sem_cp2_ex03.py

--- pcp_1sem_cp2_ex03.py
+++ pcp_1sem_cp2_ex03.py
@@ -9,18 +9,18 @@ if cp1<cp2 and cp1<cp3:
     mediasempeso1 = (cp2 + cp3 + sp1 + sp2)/4
     mediapeso= mediasempeso1*0.4 + GS*0.6
     print()
-    print(f"A sua média sem os pesos é {mediasempeso1:.2f}")
-    print(f'A sua média com os pesos é {mediapeso:.2f}')
+    print(f"A sua média sem os pesos é {mediasempeso1:.1f}")
+    print(f'A sua média com os pesos é {mediapeso:.1f}')
 if cp2<cp1 and cp2<cp3:
     mediasempeso2 = (cp1 + cp3 + sp1 + sp2)/4
     mediapeso2 = mediasempeso2 * 0.4 + (GS * 0.6)
     print()
-    print(f"A sua média sem os pesos é {mediasempeso2:.2f}")
-    print(f'A sua média com os pesos é {mediapeso2:.2f}')
+    print(f"A sua média sem os pesos é {mediasempeso2:.1f}")
+    print(f'A sua média com os pesos é {mediapeso2:.1f}')
 if cp3<cp1 and cp3<cp2:
     mediasempeso3 = (cp1 + cp2 + sp1 + sp2)/4
     mediapeso3 = mediasempeso3 * 0.4 + (GS * 0.6)
     print()
-    print(f"A sua média sem os pesos é {mediasempeso3:.2f}")
-    print(f'A sua média com os pesos é {mediapeso3:.2f}')
+    print(f"A sua média sem os pesos é {mediasempeso3:.1f}")
+    print(f'A sua média com os pesos é {mediapeso3:.1f}')
     #aaaaaaaaaaaaaaa
